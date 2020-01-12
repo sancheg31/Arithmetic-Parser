c1043b97fd9edd53c4368c1f2bc1452b6268ddc0
Alexander Kushnirenko <kushnirenko.alexander784@gmail.com>
2020-01-12 20:01:15 +0200

    Creating TesterWindow for better checking

--- TesterWindow.cpp
+++ TesterWindow.cpp
@@ -1,14 +1,20 @@
 #include "TesterWindow.h"
 #include "ui_TesterWindow.h"
 
-TesterWindow::TesterWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::TesterWindow)
-{
+TesterWindow::TesterWindow(IParserTemplateMethod* p, QWidget *parent) :
+    QMainWindow(parent), ui(new Ui::TesterWindow), parser(p) {
     ui->setupUi(this);
+    ui->result->setDigitCount(MAX_DIGIT_COUNT);
 }
 
-TesterWindow::~TesterWindow()
-{
+TesterWindow::~TesterWindow() {
     delete ui;
+}
+
+
+void TesterWindow::parseFormClicked() {
+    QString str = ui->form->toPlainText();
+    QVariant result = parser->parse(str);
+    if (result.isValid())
+        ui->result->display(result.toString());
 }
--- TesterWindow.h
+++ TesterWindow.h
@@ -2,17 +2,26 @@
 
 #include <QMainWindow>
 
+#include "Parser.h"
+#include "IParserTemplateMethod.h"
+
 namespace Ui {
 class TesterWindow;
 }
 
-class TesterWindow : public QMainWindow {
+class TesterWindow : public QMainWindow
+{
     Q_OBJECT
 
 public:
-    explicit TesterWindow(QWidget *parent = nullptr);
+    explicit TesterWindow(IParserTemplateMethod*, QWidget *parent = nullptr);
     ~TesterWindow();
 
+private slots:
+    void parseFormClicked();
+
 private:
+    const int MAX_DIGIT_COUNT = 16;
+    IParserTemplateMethod* parser;
     Ui::TesterWindow *ui;
 };
