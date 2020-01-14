0c2239a779ddea93ecf9de4afbd597757cb40f0f
Alexander Kushnirenko <kushnirenko.alexander784@gmail.com>
2020-01-14 14:43:35 +0200

    finished ValidatorContainer

--- ValidatorContainer.cpp
+++ ValidatorContainer.cpp
@@ -1,5 +1,70 @@
 #include "ValidatorContainer.h"
 
-ValidatorContainer::ValidatorContainer() {
+ValidatorContainer::ValidatorContainer() { }
 
+ValidatorContainer::ValidatorContainer(const ValidatorContainer& v) {
+    copyContainer(v.validators);
+}
+
+void ValidatorContainer::copyContainer(const container_type& cont) {
+    for (const auto & it: cont)
+       validators.push_back(it->clone());
+}
+
+ValidatorContainer::ValidatorContainer(std::initializer_list<value_type>& list) {
+    for (const auto & it: list)
+       validators.push_back(it->clone());
+}
+
+ValidatorContainer& ValidatorContainer::operator=(const ValidatorContainer& ob) {
+    copyContainer(ob.validators);
+    return *this;
+}
+
+auto ValidatorContainer::insert(value_type validator) -> iterator {
+    return validators.insert(validators.end(), validator->clone());
+}
+
+auto ValidatorContainer::toList() const -> QList<value_type> {
+    return validators;
+}
+
+int ValidatorContainer::remove(value_type v) {
+    return validators.removeAll(v);
+}
+
+bool ValidatorContainer::contains(value_type v) const {
+    return validators.contains(v);
+}
+
+int ValidatorContainer::size() const {
+    return validators.size();
+}
+
+bool ValidatorContainer::isEmpty() const {
+    return validators.isEmpty();
+}
+
+auto ValidatorContainer::begin() -> iterator {
+    return validators.begin();
+}
+
+auto ValidatorContainer::begin() const -> const_iterator {
+    return validators.begin();
+}
+
+auto ValidatorContainer::end() -> iterator {
+    return validators.end();
+}
+
+auto ValidatorContainer::end() const -> const_iterator {
+    return validators.end();
+}
+
+auto ValidatorContainer::cbegin() const -> const_iterator {
+    return validators.cbegin();
+}
+
+auto ValidatorContainer::cend() const -> const_iterator {
+    return validators.cend();
 }
--- ValidatorContainer.h
+++ ValidatorContainer.h
@@ -6,16 +6,38 @@
 
 class ValidatorContainer
 {
+protected:
+    using container_type = QList<Validator*>;
+
 public:
-    using value_type = Validator;
-    using iterator = QVector<Validator*>::iterator;
-    using const_iterator = QVector<Validator*>::const_iterator;
+    using value_type = Validator*;
+    using iterator = container_type::iterator;
+    using const_iterator = container_type::const_iterator;
 
     ValidatorContainer();
     ValidatorContainer(const ValidatorContainer&);
     ValidatorContainer(std::initializer_list<value_type>& list);
     ValidatorContainer& operator=(const ValidatorContainer&);
 
+    iterator insert(value_type);
+    int remove(value_type);
+
+    QList<value_type> toList() const;
+    bool contains(value_type) const;
+    int size() const;
+    bool isEmpty() const;
+
+    iterator begin();
+    const_iterator begin() const;
+    iterator end();
+    const_iterator end() const;
+
+    const_iterator cbegin() const;
+    const_iterator cend() const;
+
+private:
+    void copyContainer(const container_type& cont);
+    container_type validators;
 
 };
 
